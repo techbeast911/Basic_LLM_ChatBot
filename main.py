62062864861f97d123786fa69fc6adeda64c8303
Tony Akalonu <teejayakalonu002@gmail.com>
2025-01-07 18:49:00 +0100

    Created templates

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 from  langchain_openai import ChatOpenAI
 from dotenv import load_dotenv, find_dotenv
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.messages import HumanMessage, SystemMessage
 
 load_dotenv(find_dotenv(),override=True)
 
@@ -8,8 +10,35 @@ if __name__ == "__main__":
     open_ai_api_key= os.getenv("OPENAI_API_KEY")
     llm = ChatOpenAI(
     model="gpt-4o-mini")
-    question= llm.invoke("Who is the president of Nigeria")
-    print(question.content)
+    # PROMPT TEMPLATE
+    # messages = [
+    #     SystemMessage("You are a doctor recommend a Treatment for this illness"),
+    #     HumanMessage("HBV")
+    # ]
+    # answer = llm.invoke(messages)
+    # print(answer.content)
+    prompt_template=ChatPromptTemplate.from_messages([("system","Translate the following from English to{language}"),
+    ("user","{text}")])
+    prompt= prompt_template.invoke({"language":"chinese","text":"My name is teejay"})
+    result = llm.invoke(prompt)
+    print(result.content)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # if __name__ == "__main__":
 #     # Fetch the OpenAI API key from environment variables
